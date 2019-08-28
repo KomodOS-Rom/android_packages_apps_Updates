35f697868b612cec6fe2f22d614c846db0613177
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2019-08-28 14:08:12 +0700

    Updates: change strings Current Version

--- app/src/main/java/org/komodo/ota/misc/Constants.java
+++ app/src/main/java/org/komodo/ota/misc/Constants.java
@@ -32,7 +32,7 @@ public class Constants {
     public static final long UPDATE_DEFAULT_FREQ = AlarmManager.INTERVAL_HALF_DAY;
     // Build vars
     public static final String CURRENT_DEVICE_NAME = "ro.komodo.device";
-    public static final String CURRENT_VERSION = "org.komodo.version";
+    public static final String CURRENT_VERSION = "ro.modversion";
     public static final String OTA_URL = "https://raw.githubusercontent.com/KomodOS-Rom/OTA/master/%s.json";
     public static final String OTA_VERSION_CODE = "pie";
     // Expressions
